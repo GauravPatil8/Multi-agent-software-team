cc77434acb6b0111bcb7ea00d06995e05bf3781c
Gaurav <patilgauravpradeep@gmail.com>
2026-09-21 20:10:13 +0530

    remove project parameter from ChatGoogleGenerativeAI initialization

--- src/llm.py
+++ src/llm.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 load_dotenv()
+
 llm = ChatGoogleGenerativeAI(
     model = os.getenv("MODEL", "gemini-3.5-flash-lite"),
     api_key = os.getenv("GEMINI_API_KEY"),
-    project = os.getenv("GEMINI_PROJECT_ID")
 )
